748c2884c5f07d6b0721a8818a4cf354f789bc8e
Minkyu Cho <minkyu@greyboxhq.com>
2023-10-05 18:54:26 +0900

    feat: add github pages workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,25 +9,15 @@ const config = {
   title: "Notifly",
   tagline: "Customer messaging software for every small business.",
   favicon: "img/favicon.ico",
-
-  // Set the production url of your site here
-  url: "https://www.notifly.tech",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: "https://docs.notifly.tech",
   baseUrl: "/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-
-  // organizationName: 'facebook', // Usually your GitHub org/user name.
-  // projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'notifly-docs',
+  organizationName: 'team-michael',
+  trailingSlash: false,
 
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ko"],
